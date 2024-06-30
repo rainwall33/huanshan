2fb4d3d0afd4b574aacd63e67f3b4817432df27d
zhangsan <zhangsan@qq.com>
2024-06-30 10:12:58 +0800

    独孤九剑

---
+++ huanshan-jianfa/src/main/java/org/example/jianfa.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class jianfa {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
